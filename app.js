48a7f9b6a88157190bce80535002df8e88e2396a
felipe-fontenele <felipe.fontenele24@gmail.com>
2025-02-03 13:42:09 -0300

    adicionar função adicionarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let campo = document.querySelector('input');
+
+function adicionarAmigo () {
+    // pega o valor do input com id = "amigo"
+    valorDoCampo = document.getElementById('amigo').value;
+
+    if (valorDoCampo == '') {
+        alert('Por favor,insira um nome.');
+    } else {
+        amigos.push(valorDoCampo);
+        campo.value = '';
+    }
+}
