ffe829d44afd58b4449d6cc0e8619fd1d25301a1
felipe-fontenele <felipe.fontenele24@gmail.com>
2025-02-03 14:29:23 -0300

    Adicionar função adicionarLi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let amigos = [];
 let campo = document.querySelector('input');
+let lista = document.getElementById('listaAmigos');
+
 
 function adicionarAmigo () {
     // pega o valor do input com id = "amigo"
@@ -9,6 +11,17 @@ function adicionarAmigo () {
         alert('Por favor,insira um nome.');
     } else {
         amigos.push(valorDoCampo);
+        adicionarLi();
         campo.value = '';
     }
+}
+
+
+function adicionarLi(){
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++ ) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+
+
 }
