7c46be931bf738198532ad5d4f5a7db785c8c3ea
felipe-fontenele <felipe.fontenele24@gmail.com>
2025-02-09 12:14:30 -0300

    adicionar função sortearAmigo

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let amigos = [];
 let campo = document.querySelector('input');
 let lista = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+
 
 
 function adicionarAmigo () {
-    // pega o valor do input com id = "amigo"
-    valorDoCampo = document.getElementById('amigo').value;
+    let valorDoCampo = document.getElementById('amigo').value;
 
+    
     if (valorDoCampo == '') {
         alert('Por favor,insira um nome.');
     } else {
@@ -23,5 +25,21 @@ function adicionarLi(){
         lista.innerHTML += `<li>${amigos[i]}</li>`;
     }
 
+
+}
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = 'Nenhum amigo disponível para o sorteio!';
+        
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+
+        lista.innerHTML = '';
+
+        resultado.innerHTML = `O amigo sorteado foi: <strong>${amigoSorteado}</strong>`;
+    }
 
 }
